8c23749f63c45dafa2c30e3489dea66612c985b5
salohiddindev1 <169024491+salohiddindev1@users.noreply.github.com>
2024-07-12 21:44:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let students = [
+  {
+    firstName: "Salohiddin",
+    age: "13 yosh",
+    city: "Uzbekistan",
+  },
+];
+
+for (let value of students) {
+  console.log(`
+${value.firstName}
+${value.age}
+${value.city}`);
+}
